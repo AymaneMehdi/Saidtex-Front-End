e851205c4aef0534589f2fa4395e151805bff839
Aymane Mehdi <yorubornin1999@gmail.com>
2026-06-24 00:15:40 +0100

    Remove duplicate footer from layout.

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Footer />
       </body>
     </html>
   );
