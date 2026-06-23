3c9bda60d3f07778103f70a9335fdda2cf110579
Aymane Mehdi <yorubornin1999@gmail.com>
2026-06-23 23:57:51 +0100

    Add Partners component and integrate it into the Home page.

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/nav";
 import Hero from "./components/hero";
 import About from "./components/about";
+import Partners from "./components/partenaires";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
       <About />
+      <Partners />
     </div>
   );
 }
