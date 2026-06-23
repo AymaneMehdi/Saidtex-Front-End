25c58bde6fe87f2a50d7a59e811952c9e563cc40
Aymane Mehdi <yorubornin1999@gmail.com>
2026-06-23 23:58:27 +0100

    Add Whyus component and integrate it into the Home page.

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Navbar from "./components/nav";
 import Hero from "./components/hero";
 import About from "./components/about";
 import Partners from "./components/partenaires";
+import Whyus from "./components/whyus";
 export default function Home() {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Partners />
+      <Whyus />
     </div>
   );
 }
